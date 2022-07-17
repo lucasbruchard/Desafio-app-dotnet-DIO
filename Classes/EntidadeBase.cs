9ec1541e26683952f0fb1ef102bf2f28a3d621d7
Lucas <lucasdebruchard@gmail.com>
2022-07-17 18:16:01 -0300

    Desafio-dotnet

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace Desafio_dotnet
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+#DESAFIO APP DE SÉRIES DOTNET
+##Esse projeto se trata do desafio de projeto da DIO para o fim de criar app de séries pelo .NET.
